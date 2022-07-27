8a06836ae9d350b1e652c5a5f71e75f3af8d4a6a
hayata19791218 <hayatakyohei@kzh.biglobe.ne.jp>
2022-07-27 16:11:57 +0900

    データのアップ

---
+++ js/common.js
@@ -0,0 +1,28 @@
+window.addEventListener('scroll',function (){
+
+  const textFade = document.querySelector('.bgappearTrigger');
+  
+  const rect1 = textFade.getBoundingClientRect().top;
+  const scroll = window.pageYOffset;
+  const offset1 = rect1 + scroll;
+  const windowHeight = window.innerHeight; 
+  
+  if(scroll > offset1 - windowHeight){
+    textFade.classList.add('textAppear');
+  }
+  else{
+    textFade.classList.remove('textAppear');
+  }
+
+  const backgroundFade = document.querySelector('.bgRLextendTrigger');
+  const rect2 = backgroundFade.getBoundingClientRect().top;
+  const offset2 = rect2 + scroll;
+  const windowHeight2 = window.innerHeight; 
+  
+  if(scroll > offset2 - windowHeight2){
+    backgroundFade.classList.add('backgroundExtend');
+  }
+  else{
+    backgroundFade.classList.remove('backgroundExtend');
+  }
+});
